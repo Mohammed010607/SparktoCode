806be3d5dbea5904740b02a3b3da4a144127af58
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-07-02 12:23:21 +0400

    Sum of Numbers 1 to N code

---
+++ C# Fundamentals Tasks/Part 2/Task 2/Sum_of_Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Sum_of_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Positive Integer: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int result = 0;
+            for (int i = 1; i <= num; i++) {
+                result += i;
+            }
+            Console.WriteLine("Result = " + result);
+        }
+    }
+}

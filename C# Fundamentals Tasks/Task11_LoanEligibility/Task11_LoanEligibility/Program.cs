79d65b5109a22d45a9fe85a35c29cf578ff2e45e
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-07-02 07:57:53 +0400

    Commiting Task 11, a Loan eligibility system

---
+++ C# Fundamentals Tasks/Task11_LoanEligibility/Task11_LoanEligibility/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task11_LoanEligibility
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Your monthly Income: ");
+            double income = double.Parse(Console.ReadLine());
+            Console.WriteLine("Do You Have an Existing Loan (Y/N): ");
+            bool loan = Console.ReadLine() == "Y";
+
+            if (age >= 21 && age <=60 && income >=400 && loan != true)
+            {
+                Console.WriteLine("You are Eligable for a Loan. ");
+            }
+            else
+            {
+                if (age < 21 || age > 60)
+                {
+                    Console.WriteLine("You are not Eligable for a Loan due to Age Restriction. ");
+                }
+                else if (income < 400)
+                {
+                    Console.WriteLine("You are not Eligable for a Loan due to Income Restriction. ");
+                }
+                else if (loan == true)
+                {
+                    Console.WriteLine("You are not Eligable for a Loan due to Existing Loan. ");
+                }
+            }
+        }
+    }
+}

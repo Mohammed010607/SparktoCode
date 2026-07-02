ca8046a36501f1bd1f8a1d0d67db0d041d6d6462
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-07-02 12:54:56 +0400

    Pass Retry Code

---
+++ C# Fundamentals Tasks/Part 2/Task 4/PasswordRetry/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PasswordRetry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string passCode = "";
+            while(passCode != "Spark2026") {
+                Console.WriteLine("Please enter the password: ");
+                passCode = Console.ReadLine();
+            }
+            Console.WriteLine("Access granted!");
+        }
+    }
+}

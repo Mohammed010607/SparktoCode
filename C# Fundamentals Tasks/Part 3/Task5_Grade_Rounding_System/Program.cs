3ad1fcd6325fb7d702d7dd6ca14ee1c79a16e4e5
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-07-05 23:36:26 +0400

    Commiting Task 5, Grade Rounding System Code

---
+++ C# Fundamentals Tasks/Part 3/Task5_Grade_Rounding_System/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task5_Grade_Rounding_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your raw exam score (e.g. 74.6): ");
+            string input = Console.ReadLine();
+
+            double score = double.Parse(input);
+            double roundedScore = Math.Round(score, 0);
+
+            if (roundedScore >= 60)
+            {
+                Console.WriteLine("Rounded Score: " + roundedScore);
+                Console.WriteLine("Result: Pass");
+            }
+            else
+            {
+                Console.WriteLine("Rounded Score: " + roundedScore);
+                Console.WriteLine("Result: Fail");
+            }
+        }
+    }
+}

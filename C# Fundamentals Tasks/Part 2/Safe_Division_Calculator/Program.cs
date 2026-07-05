12d8d54b9ae58de1522f7c3da29a01f041514b08
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-07-05 10:58:06 +0400

    Commiting Safe Division Calculator Code

---
+++ C# Fundamentals Tasks/Part 2/Safe_Division_Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Safe_Division_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the First Number: ");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the Second Number: ");
+                int num2 = int.Parse(Console.ReadLine());
+
+                int result = num1 / num2;
+
+                Console.WriteLine("Result: " + result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Error: Division by zero is not allowed.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Invalid input. Please enter valid integers.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected error occurred.");
+            }
+        }
+    }
+}

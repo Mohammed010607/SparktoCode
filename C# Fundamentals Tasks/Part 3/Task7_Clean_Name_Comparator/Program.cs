37bb90a33eca4df96b91c43051a10088a70df6cf
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-07-05 16:08:11 +0400

    Commiting Code for Name Comparator

---
+++ C# Fundamentals Tasks/Part 3/Task7_Clean_Name_Comparator/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task7_Clean_Name_Comparator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Same Name Twice (with different casings and spacing): ");
+            Console.WriteLine("Enter the Name: ");
+            string name1 = Console.ReadLine();
+            Console.WriteLine("Enter the Name Again: ");
+            string name2 = Console.ReadLine();
+
+            name1 = name1.ToUpper().Trim();
+            name2 = name2.ToUpper().Trim();
+            
+            if(name1 == name2)
+            {
+                Console.WriteLine("The Names are the Same");
+            }
+            else
+            {
+                Console.WriteLine("The Names are Different");
+            }
+        }
+    }
+}

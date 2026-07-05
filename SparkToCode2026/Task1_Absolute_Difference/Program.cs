707f258f7239f107bb272a68ca3e0fdfe6caeddb
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-07-05 14:28:04 +0400

    Completion of Absolute Difference

---
+++ SparkToCode2026/Task1_Absolute_Difference/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task1_Absolute_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a First Number: ");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a Second Number: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double result = Math.Abs(num1 - num2);
+            Console.WriteLine("The Absolute Difference Between the Two Numbers is: " + result);
+        }
+    }
+}

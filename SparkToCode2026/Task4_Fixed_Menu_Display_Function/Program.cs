1a0e8e9bf16fc77e4c0a5c221e31d64b8ef893f6
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-07-06 17:16:02 +0400

    Commiting Code for Fixed Menu Function (Task 4)

---
+++ SparkToCode2026/Task4_Fixed_Menu_Display_Function/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task4_Fixed_Menu_Display_Function
+{
+    internal class Program
+    {
+        static void DisplayMenu(){
+            Console.WriteLine("1. Start");
+            Console.WriteLine("2. Help");
+            Console.WriteLine("3. Exit");
+        }
+
+
+        static void Main(string[] args)
+        {
+            DisplayMenu();
+
+        }
+    }
+}

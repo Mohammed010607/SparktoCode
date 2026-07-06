04be053f3cd7d26b1155ed893a73140b915193c8
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-07-06 17:43:30 +0400

    Commiting Rectange Area & Perimiter Function (Task 6)

---
+++ SparkToCode2026/Task6_Rectangle_Area_&_Perimiter_Function/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task6_Rectangle_Area___Perimiter_Function
+{
+    internal class Program
+    {
+        static double CalculateArea(double length, double width) {
+
+            return length * width;
+        }
+
+        static double CalculatePerimiter(double length, double width) { 
+        
+            return 2*(length + width);
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Lenght of the Rectangle: ");
+            double l = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Width of the Rectangle: ");
+            double w = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("The Area is: "+CalculateArea(l, w));
+            Console.WriteLine("The Perimiter is: "+CalculatePerimiter(l, w));
+        }
+    }
+}

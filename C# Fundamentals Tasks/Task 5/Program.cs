f1af026a06f86d250f92b1d42f58ef8683a7a084
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-07-07 15:44:43 +0400

    Completed Q1 & Q2 in Task 5

--- C# Fundamentals Tasks/Task 5/Program.cs
+++ C# Fundamentals Tasks/Task 5/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+            //Task 1 (Fixed Grade Array)
+            int[] grades = new int[5];
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.WriteLine("Enter Grade: " + (i + 1) + ": " );
+                grades[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Your Grades Are: ");
+            foreach (int grade in grades) {
+                
+                Console.WriteLine(grade);
+            }
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+            */
+
+            //Task 2 (Dynamic To-Do List)
+            List<string>toDoList = new List<string>();
+
+            for(int i = 0; i< 5; i++)
+            {
+                Console.WriteLine("Enter task " + (i + 1) + ": " );
+                toDoList.Add(Console.ReadLine());
+            }
         }
     }
 }

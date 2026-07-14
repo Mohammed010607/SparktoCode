b89242e5dd8d148d9ce2d1bd29035f4e595325af
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-07-14 10:42:18 +0400

    Room Class Code Completion

--- C# Fundamentals Tasks/TS-OOP-01/HotelManagementSystem/Room.cs
+++ C# Fundamentals Tasks/TS-OOP-01/HotelManagementSystem/Room.cs
@@ -6,5 +6,25 @@ namespace HotelManagementSystem
 {
     internal class Room
     {
+        public int roomNumber {get; private set; }
+        public string roomType {get; private set;}
+        public double pricePerNight{ get; set;}
+        public bool isAvailable { get; set;}
+
+        public Room(int roomNumber,string roomType,double pricePerNight)
+        {
+            this.roomNumber = roomNumber;
+            this.roomType = roomType;
+            this.pricePerNight = pricePerNight;
+            isAvailable = true;
+        }
+
+        public void displayRoom()
+        {
+            Console.WriteLine(roomNumber);
+            Console.WriteLine(roomType);
+            Console.WriteLine(pricePerNight);
+            Console.WriteLine(isAvailable);
+        }
     }
 }

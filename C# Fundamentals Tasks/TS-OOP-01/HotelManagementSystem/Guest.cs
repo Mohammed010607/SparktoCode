c36fb8aeba83c1b9b35b788943d8a833538dcdb2
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-07-14 11:29:45 +0400

    Guest Class Code Completion

--- C# Fundamentals Tasks/TS-OOP-01/HotelManagementSystem/Guest.cs
+++ C# Fundamentals Tasks/TS-OOP-01/HotelManagementSystem/Guest.cs
@@ -6,5 +6,33 @@ namespace HotelManagementSystem
 {
     internal class Guest
     {
+        public string guestID {  get; private set; }
+        public string guestName { get; private set; }
+        public string roomNumber { get; set; }
+        public string checkInDate { get; private set; }
+        public int totalNights { get; set; }
+        
+        public Guest(string guestID, string guestName, string checkInDate, int totalNights)
+        {
+            this.guestID = guestID;
+            this.guestName = guestName;
+            this.checkInDate = checkInDate;
+            this.totalNights = totalNights;
+            roomNumber = "Not Assigned";
+        }
+
+        public void displayGuest()
+        {
+            Console.WriteLine(guestID);
+            Console.WriteLine(guestName);
+            Console.WriteLine(roomNumber);
+            Console.WriteLine(checkInDate);
+            Console.WriteLine(totalNights);
+
+        }
+        public double calculateTotalCost(double pricePerNight)
+        {
+            return pricePerNight * totalNights;
+        }
     }
 }
